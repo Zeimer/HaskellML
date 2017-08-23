749e77a9ee00cdf5a08601a0478f349b277aaa24
wkolowski <wkolowski@protonmail.com>
2017-08-23 23:53:43 +0200

    Wrote a utility function for reading three datasets at once (for training, validation and testing). Moved the code for plotting training curves to a separate function.

--- ex5/Ex1.hs
+++ ex5/Ex1.hs
@@ -17,15 +17,38 @@ import LinearRegression
 readMatrG :: String -> FilePath -> IO (Matrix R)
 readMatrG sep path = liftM (fromLists . map (map read . splitOn sep . tail) . lines) $ readFile path
 
-draw name datapoints = toFile def (name ++ ".png") $ do
-		layout_title .= name
-		setColors [opaque red]
-		plot (points "data, obviously" datapoints)
+read3 :: FilePath -> FilePath -> FilePath -> IO (Matrix R, Vector R, Matrix R, Vector R, Matrix R, Vector R)
+read3 p1 p2 p3 = do
+	(x1, y1) <- liftM getXY $ readMatrG " " p1
+	(x2, y2) <- liftM getXY $ readMatrG " " p2
+	(x3, y3) <- liftM getXY $ readMatrG " " p3
 
-plotData :: IO ()
-plotData = do
-	m <- readMatrG " " "ex5/train.txt"
-	draw "ex5/data" $ map (\[x, y] -> (x, y)) (toLists m)
+	return (x1, y1, x2, y2, x3, y3)
+
+-- Plot training and validation curves.
+plotCurves ::
+	String ->					-- Name under which the file is to be saved.
+	Matrix R -> Vector R ->				-- Train data.
+	Matrix R -> Vector R ->				-- Validation data.
+	(Matrix R -> Vector R -> Vector R -> R) ->	-- Cost function.
+	((Matrix R, Vector R) -> Vector R) ->		-- Parameter learning function.
+	IO ()
+
+plotCurves name x_train y_train x_val y_val cost f = do
+	let
+		xs_train = map (\n -> takeRows n x_train) [1..(rows x_train)]
+		ys_train = map (\n -> fromList . take n $ toList y_train) [1..(rows x_train)]
+
+		θs = map f (zip xs_train ys_train)
+
+		training_curve = zip ([1..] :: [Int]) $ map (\(x, y, θ) -> cost x y θ) $ zip3 xs_train ys_train θs
+		validation_curve = zip ([1..] :: [Int]) $ map (cost x_val y_val) θs
+
+	toFile def (name ++ ".png") $ do
+		layout_title .= name
+		setColors [opaque blue, opaque green]
+		plot (line "training curve" [training_curve])
+		plot (line "validation curve" [validation_curve])
 
 -- Step through the whole exercise.
 ex5 :: IO ()
@@ -33,8 +56,6 @@ ex5 = do
 	m_train <- readMatrG " " "ex5/train.txt"
 	m_val <- readMatrG " " "ex5/val.txt"
 	m_test <- readMatrG " " "ex5/test.txt"
-
-	--draw "ex5/data" $ map (\[a, b] -> (a, b)) (toLists m_train)
 	
 	let
 		(x_train, y_train) = getXY m_train
@@ -54,22 +75,5 @@ ex5 = do
 		plot (points "data, obviously" datapoints)
 		plot (line "fit" [g])
 
-	-- Training curve.
-	let
-		xs_train = map (\n -> takeRows n x_train) [1..(rows x_train)]
-		ys_train = map (\n -> fromList . take n $ toList y_train) [1..(rows x_train)]
-
-		θs = map (\(x, y) -> descentAGV 4e-1 (vector [0, 0]) (cost x y) (cost' x y)) (zip xs_train ys_train)
-
-		train_curve = map (\(x, y, θ) -> cost x y θ) $ zip3 xs_train ys_train θs
-		val_curve = map (cost x_val y_val) θs
-
-		g_train = zip ([1..] :: [Int]) train_curve
-		g_val = zip ([1..] :: [Int]) val_curve
-
-	toFile def ("ex5/train_curve.png") $ do
-		layout_title .= name
-		setColors [opaque blue, opaque green]
-		plot (line "train curve" [g_train])
-		plot (line "val curve" [g_val])
-	
+	plotCurves "ex5/curves" x_train y_train x_val y_val cost $
+		\(x, y) -> descentAGV 4e-1 (vector [0, 0]) (cost x y) (cost' x y)
